4efcfb93e7c2e04f2db1fa83046070f311f272bb
gianluca <gianlucachiap@gmail.com>
2020-01-01 15:45:17 +0100

    Update notifier

---
+++ src/hooks/init/checkUpdate.ts
@@ -0,0 +1,13 @@
+import { Hook } from '@oclif/config';
+const updateNotifier = require('update-notifier');
+const pkg = require('../../../package.json');
+
+const hook: Hook<'init'> = async function(opts) {
+  const notifier = updateNotifier({
+    pkg,
+    updateCheckInterval: 1000 * 60 * 60 * 24 * 7,
+  });
+  notifier.notify();
+};
+
+export default hook;
